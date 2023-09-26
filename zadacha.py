e7cc2ada0813c24cc7f9e798b53a68d3644093ed
Comp022023 <kyurinova@inbox.ru>
2023-09-26 12:40:21 +0300

    Перекраска кнопок

--- zadacha.py
+++ zadacha.py
@@ -1,7 +1,34 @@
-#Задание для моих коллег:
-#задание для дениса: Сделать фотографию и расположить ее слева как будто профиль человека,
-#сделать название компании справа и описание ее(добавить текст, пару предолжений)
-
 #Задание для Костяна-коллеги : Сделать три кнопки и расположить их в самом низу,
 # у кнопок должен быть какой-либо эффект, либо кнопка меняется либо что-то выходит на экран
 
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.geometry('400x300+700+200')
+
+def red():
+    styles = ttk.Style()
+    styles.configure("BW.TLabel", background="red")
+    btns['style'] = "BW.TLabel"
+
+btns = ttk.Button(text='Покраска в красный', command=red) #Кнопкав нижнем левом углу
+btns.place(relx=.0, rely=1, anchor='sw')
+
+def purple():
+    styles = ttk.Style()
+    styles.configure("BW.TLabel", background="purple")
+    btn['style'] = "BW.TLabel"
+
+btn = ttk.Button(text='Покраска в фиолетовый', command=purple) #Кнопка внизу центра
+btn.place(relx=0.5, rely=1, anchor='s')
+
+def black():
+    style = ttk.Style()
+    style.configure("BW.TLabel", foreground="white", background="black")
+    buttonTTK['style'] = "BW.TLabel"
+
+buttonTTK = ttk.Button(text="Покраска в чёрный", command=black) #Кнопка в правом нижнем углу
+buttonTTK.place(relx=1, rely=1, anchor='se')
+
+root.mainloop()
